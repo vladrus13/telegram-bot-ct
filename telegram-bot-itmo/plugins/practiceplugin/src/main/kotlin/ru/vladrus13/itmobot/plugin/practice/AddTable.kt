8d7dd54553b19383ffa721184d4da0821243bc05
Alexey Grunskii <you@example.com>
2023-04-24 22:23:42 +0300

    Save things for correct work

---
+++ telegram-bot-itmo/plugins/practiceplugin/src/main/kotlin/ru/vladrus13/itmobot/plugin/practice/AddTable.kt
@@ -0,0 +1,78 @@
+package ru.vladrus13.itmobot.plugin.practice
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import com.google.api.client.http.HttpRequestInitializer
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.SheetsScopes
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.objects.Update
+import ru.vladrus13.itmobot.bean.User
+import ru.vladrus13.itmobot.command.Foldable
+import ru.vladrus13.itmobot.command.Menu
+import java.util.*
+
+class AddTable(override val parent: Menu) : Menu(parent) {
+    override val childes: Array<Foldable> = arrayOf()
+
+    override fun menuHelp() = "Пункт создания таблицы"
+
+    override val name: String
+        get() = "Создать новую таблицу (введите название)"
+    override val systemName: String
+        get() = "makeTable"
+
+    override fun isAccept(update: Update): Boolean = update.message.text == name
+
+    /**
+     * ublic static String createSpreadsheet(String title) throws IOException {
+     *         /* Load pre-authorized user credentials from the environment.
+     *            TODO(developer) - See https://developers.google.com/identity for
+     *             guides on implementing OAuth2 for your application. */
+     *     GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+     *         .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+     *     HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
+     *         credentials);
+     *
+     *     // Create the sheets API client
+     *     Sheets service = new Sheets.Builder(new NetHttpTransport(),
+     *         GsonFactory.getDefaultInstance(),
+     *         requestInitializer)
+     *         .setApplicationName("Sheets samples")
+     *         .build();
+     *
+     *     // Create new spreadsheet with a title
+     *     Spreadsheet spreadsheet = new Spreadsheet()
+     *         .setProperties(new SpreadsheetProperties()
+     *             .setTitle(title));
+     *     spreadsheet = service.spreadsheets().create(spreadsheet)
+     *         .setFields("spreadsheetId")
+     *         .execute();
+     *     // Prints the new spreadsheet id
+     *     System.out.println("Spreadsheet ID: " + spreadsheet.getSpreadsheetId());
+     *     return spreadsheet.getSpreadsheetId();
+     *   }
+     */
+
+    override fun get(update: Update, bot: TelegramLongPollingBot, user: User) {
+        if (standardCommand(update, bot, user)) return
+        val text: String = update.message.text
+        if (text.isBlank()) {
+            unknownCommand(bot, user)
+        }
+
+//        val credentials: GoogleCredential = GoogleCredential.getApplicationDefault()
+//            .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS))
+//
+//        val service: Sheets = Sheets.Builder(NetHttpTransport(
+//            GsonFactory.getDefaultInstance(),
+//        ))
+
+        user.send(
+            bot = bot,
+            text = text,
+            replyKeyboard = getReplyKeyboard(user)
+        )
+    }
+}
--- telegram-bot-itmo/plugins/practiceplugin/src/main/kotlin/ru/vladrus13/itmobot/plugin/practice/PracticeCommand.kt
+++ telegram-bot-itmo/plugins/practiceplugin/src/main/kotlin/ru/vladrus13/itmobot/plugin/practice/PracticeCommand.kt
@@ -5,29 +5,25 @@ import org.telegram.telegrambots.meta.api.objects.Update
 import ru.vladrus13.itmobot.bean.Chat
 import ru.vladrus13.itmobot.bean.User
 import ru.vladrus13.itmobot.command.Command
+import ru.vladrus13.itmobot.command.Foldable
 import ru.vladrus13.itmobot.command.Menu
 
-class PracticeCommand(override val parent: Menu) : Command() {
-    override fun help(): String = "Этот плагин позволяет создать таблицу для отметки задач на практиках"
+class PracticeCommand(override val parent: Menu) : Menu(parent) {
+    override val childes: Array<Foldable> = arrayOf(
+        AddTable(this)
+    )
+
+    override fun menuHelp() = "Этот плагин позволяет создать таблицу для отметки задач на практиках"
 
     override val name: String
         get() = "Создать таблицу"
     override val systemName: String
         get() = "practice"
 
-    override fun isAccept(update: Update): Boolean = update.message.text!! == name
+    override fun isAccept(update: Update): Boolean = update.message.text == name
 
     override fun get(update: Update, bot: TelegramLongPollingBot, user: User) {
-        user.send(
-            bot = bot,
-            text = update.message.text ?: "---"
-        )
-    }
-
-    override fun get(update: Update, bot: TelegramLongPollingBot, user: User, chat: Chat) {
-        chat.send(
-            bot = bot,
-            text = update.message.text ?: "---"
-        )
+        if (classicUpdate(update, bot, user)) return
+        unknownCommand(bot, user)
     }
 }
