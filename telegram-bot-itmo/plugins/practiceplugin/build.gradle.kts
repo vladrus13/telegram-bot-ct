6a05a06120528f31f99af749091e246064a4a66e
Alexey Grunskii <you@example.com>
2023-04-24 22:23:42 +0300

    Save things for correct work

--- telegram-bot-itmo/plugins/practiceplugin/build.gradle.kts
+++ telegram-bot-itmo/plugins/practiceplugin/build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 
 dependencies {
     implementation("org.telegram:telegrambots:5.2.0")
+    implementation("com.google.api-client:google-api-client:1.31.5")
+    implementation("com.google.oauth-client:google-oauth-client:1.31.5")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.23.0")
+    implementation("com.google.apis:google-api-services-drive:v3-rev110-1.23.0")
+
+    implementation("com.google.apis:google-api-services-sheets:v4-rev614-1.18.0-rc")
 
     implementation(project(":skeleton"))
 }
