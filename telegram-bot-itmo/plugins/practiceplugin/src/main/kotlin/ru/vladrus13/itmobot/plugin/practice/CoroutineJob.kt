0def44c0eb01f393f2b459440f4d826e49f5d0c8
MegaVerkruzo <robochat@mail.ru>
2023-09-17 22:34:44 +0300

    Add newFunctionality

--- telegram-bot-itmo/plugins/practiceplugin/src/main/kotlin/ru/vladrus13/itmobot/plugin/practice/CoroutineJob.kt
+++ telegram-bot-itmo/plugins/practiceplugin/src/main/kotlin/ru/vladrus13/itmobot/plugin/practice/CoroutineJob.kt
@@ -1,5 +1,6 @@
 package ru.vladrus13.itmobot.plugin.practice
 
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -7,9 +8,16 @@ import ru.vladrus13.itmobot.database.DataBaseParser
 import ru.vladrus13.itmobot.google.GoogleTableResponse
 import ru.vladrus13.itmobot.plugin.practice.googleapi.GoogleSheet
 import ru.vladrus13.itmobot.plugin.practice.parsers.neerc.NeercParserInfo
+import ru.vladrus13.itmobot.plugin.practice.transfer.TransferData.Companion.transferFCSToLastName
+import ru.vladrus13.itmobot.plugin.practice.transfer.TransferData.Companion.transferStudentTableToTeacher
+import ru.vladrus13.itmobot.properties.InitialProperties
+import java.util.Objects.deepEquals
+import java.util.logging.Logger
 
 class CoroutineJob {
     companion object {
+        private val logger: Logger = InitialProperties.logger
+
         const val NEERC_JOB = 1L
 
         fun addTask(jobId: Long, sourceLink: String, tableLink: String, tableId: String, userId: Long) {
@@ -33,28 +41,42 @@ class CoroutineJob {
                     allSheetTables
                         .drop(tableIndex)
                         .take(batchSize)
-                        .forEach { row ->
-                            val jobId = row[SheetJobTable.jobId]
-                            val sourceLink = row[SheetJobTable.sourceLink]
-                            val tableId = row[SheetJobTable.tableId]
-                            when (jobId) {
-                                NEERC_JOB -> {
-                                    val actualTasks: List<String> = NeercParserInfo(sourceLink).getTasks()
-                                    val googleSheet = GoogleSheet(GoogleTableResponse.createSheetsService(), tableId)
-
-                                    val currentTasks = googleSheet.getTasksList().flatten()
-
-                                    if (currentTasks.isEmpty() && actualTasks.isNotEmpty() || actualTasks.last() != currentTasks.last()) {
-                                        googleSheet.generateSheet(
-                                            actualTasks.subList(currentTasks.size, actualTasks.size)
-                                        )
-                                    }
-                                }
-                            }
-                        }
+                        .forEach(CoroutineJob::runTask)
                 }
             }
             return nextIndex
+        }
+
+        private fun runTask(row: ResultRow) {
+            val jobId = row[SheetJobTable.jobId]
+            val sourceLink = row[SheetJobTable.sourceLink]
+            val tableId = row[SheetJobTable.tableId]
+            when (jobId) {
+                NEERC_JOB -> runNeercTask(sourceLink, tableId)
+            }
+        }
+
+        private fun runNeercTask(sourceLink: String, tableId: String) {
+            val actualTasks: List<String> = NeercParserInfo(sourceLink).getTasks()
+            val googleSheet = GoogleSheet(GoogleTableResponse.createSheetsService(), tableId)
+
+            logger.info("Indicator of creating new teacher sheet: ${googleSheet.generateTeacherSheet()}")
+
+            val fcsTasksWithMarks = googleSheet.getFCSTasksWithMarks()
+            val currentTasks: List<String> = fcsTasksWithMarks.firstOrNull()?.drop(1) ?: listOf()
+            val teacherSheetBody: List<List<String>> =
+                if (fcsTasksWithMarks.isEmpty()) listOf()
+                else fcsTasksWithMarks.transferStudentTableToTeacher().transferFCSToLastName()
+
+            // Add newList
+            if (currentTasks.isEmpty() && actualTasks.isNotEmpty() || actualTasks.last() != currentTasks.last()) {
+                googleSheet.generateSheet(actualTasks.subList(currentTasks.size, actualTasks.size))
+            }
+
+            // Update teacher sheet List
+            if (teacherSheetBody.isNotEmpty() && !deepEquals(googleSheet.getTeacherList(), teacherSheetBody)) {
+                googleSheet.updateFields(teacherSheetBody)
+            }
         }
     }
 }
